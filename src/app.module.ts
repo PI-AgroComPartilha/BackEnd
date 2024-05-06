8adfff9c90d5afad3f25787530e056a78b03f5e8
Vitor Oliveira. <caulicons@gmail.com>
2024-05-06 12:33:03 -0300

    atualizando config para o render

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,26 +7,25 @@ import { Categorias } from "./categorias/entities/categoria.entity";
 import { Produto } from "./produtos/entities/produto.entity";
 import { ProdutoModule } from "./produtos/produto.module";
 import { Usuario } from "./usuarios/entities/usuario.entity";
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { UsuarioModule } from './usuarios/usuarios.module';
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import { UsuarioModule } from "./usuarios/usuarios.module";
 import { DevService } from "./auth/data/services/dev.service";
-
+import { ProdService } from "./auth/data/services/prod.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     ProdutoModule,
     CategoriasModule,
     UsuarioModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [],
 })
-
 export class AppModule {}
