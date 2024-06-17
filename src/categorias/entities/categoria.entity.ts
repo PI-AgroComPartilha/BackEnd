81689155cb62fb623e3a31febbf38451e5bedd97
Vitor Oliveira. <caulicons@gmail.com>
2024-06-17 19:33:14 -0300

    fix: category entity problem

--- src/categorias/entities/categoria.entity.ts
+++ src/categorias/entities/categoria.entity.ts
@@ -2,6 +2,19 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Produto } from "../../produtos/entities/produto.entity";
 import { ApiProperty } from "@nestjs/swagger";
 
+class ProdutoDTO {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  nome: string;
+  @ApiProperty()
+  descricao: string;
+  @ApiProperty()
+  foto: string;
+  @ApiProperty()
+  quantidade: number;
+}
+
 @Entity({ name: "tb_categorias" })
 export class Categoria {
   @PrimaryGeneratedColumn()
@@ -12,7 +25,7 @@ export class Categoria {
   @Column({ nullable: false, length: 255 })
   nome: string;
 
-  @ApiProperty({ type: () => Produto })
+  @ApiProperty({ type: () => [ProdutoDTO] })
   @OneToMany(() => Produto, (produtos) => produtos.categorias)
   produtos: Produto[];
 }
